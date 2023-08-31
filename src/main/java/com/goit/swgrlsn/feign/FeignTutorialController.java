81e62588eab34edf3e1152f24b1ab2e047ee5bd9
petroz <petroz@ext.inditex.com>
2023-08-31 11:46:57 +0300

    fix feign

--- src/main/java/com/goit/swgrlsn/feign/FeignTutorialController.java
+++ src/main/java/com/goit/swgrlsn/feign/FeignTutorialController.java
@@ -2,14 +2,14 @@ package com.goit.swgrlsn.feign;
 
 import com.goit.swgrlsn.controllers.configs.FeignConfig;
 import com.goit.swgrlsn.dto.PageDto;
+import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "custome", url = "${url.random}", 
     configuration = FeignConfig.class)
 public interface FeignTutorialController  {
   @GetMapping
+  @RequestLine("GET")
   PageDto getPage();
 }
